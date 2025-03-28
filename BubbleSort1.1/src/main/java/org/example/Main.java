8ecf8cc961040a0690cb64fd56f5427d7da63685
ufukk12 <ufuk.ozdemir@galata.edu.tr>
2025-03-28 14:53:37 +0300

    Bubble Sort

---
+++ BubbleSort1.1/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Bubble sort ikili kıyaslama yapar.
+         *
+         * dizi[i] < dizi[i+1];
+         * i++
+         */
+
+        int uzunluk;
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Lütfen sıralanacak dizi uzunluğu girin: ");
+        uzunluk = scn.nextInt();
+
+
+
+
+        BubbleSort sort = new BubbleSort();
+
+        int dizi[] = sort.diziÜret(uzunluk);
+
+        int sıralanmısDizi[] = sort.sort(dizi , 0);
+
+        for (int i = 0; i < dizi.length; i++){
+            System.out.println(sıralanmısDizi[i]);
+        }
+
+//a
+
+    }
+}
